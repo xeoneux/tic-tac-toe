7adff93d85f4423884c414953daa149620fd20fc
Aayush Kapoor <xeoneux@icloud.com>
2017-10-02 11:57:18 +0530

    Save state in Board

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
@@ -21,8 +21,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
